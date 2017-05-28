a1847cbdcadcb400ed69cbcdf55aa4aedaf907a4
Administrador <Sergio.escuder5@gmail.com>
2017-05-28 17:35:40 +0200

    falta probar que lea bien el mapa la clase battlefield

--- ClashOfEntios/battlefield.cpp
+++ ClashOfEntios/battlefield.cpp
@@ -4,6 +4,40 @@
 
 battlefield::battlefield()
 {
+	rows = 74;
+	columns = 36;
+	field = new char *[rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		field[i] = new char[columns];
+	}
+	
+
+	//std::ifstream level("default.cfg");
+	FILE *level;
+	fopen("default.cfg", "r");
+
+		for (int i = 0; i <= rows; i++)
+		{
+			for (int j = 0; j < columns; i++)
+			{
+				field[i][j] = fgetc(level);				
+			}
+		}
+		 
+		fclose(level);
+
+
+}
+
+char battlefield::getCell(int x, int y)
+{
+	return field[x][y];
+}
+void battlefield::setCell(int x, int y, char element)
+{
+	field[x][y] = element;
 }
 
 
--- ClashOfEntios/battlefield.h
+++ ClashOfEntios/battlefield.h
@@ -1,8 +1,22 @@
 #pragma once
+#include<vector>
+#include<iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
 class battlefield
 {
+private:
+	char **field;
+	int rows;
+	int columns;
+
 public:
 	battlefield();
+	char getCell(int x,int y);
+	void setCell(int x, int y, char elment);
 	~battlefield();
 };
 
